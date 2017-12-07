64107350b118e8c00968f830804b72a4eab640db
Trương Minh Quý <truongminhquy46@gmail.com>
2017-12-07 14:24:01 +0700

    Add files via upload

---
+++ Believer/MyLoader.cs
@@ -0,0 +1,56 @@
+﻿namespace Believer_Loader
+{
+    #region
+
+    using System;
+    using System.IO;
+    using System.Reflection;
+	
+    using Aimtec.SDK.Events;
+
+    #endregion
+
+    public static class MyLoader
+    {
+        public static void Main()
+        {
+            GameEvents.GameStart += () =>
+            {
+                var loader = new Loader();
+            };
+        }
+
+        private class Loader
+        {
+            public Loader()
+            {
+                var link = Environment.GetEnvironmentVariable("LocalAppData");
+                var filePath = link + @"\AimtecLoader\Data\System\BelieverCore.dll";
+
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+
+                var prdll = Properties.Resources.Believer;
+                using (var fs = new FileStream(filePath, FileMode.Create))
+                {
+                    fs.Write(prdll, 0, prdll.Length);
+                }
+
+                var a = Assembly.LoadFrom(filePath);
+                var myType = a.GetType("Believer.BelieverLoader");
+                var main = myType.GetMethod("Main", BindingFlags.Public | BindingFlags.Static);
+
+                if (main != null)
+                {
+                    main.Invoke(null, null);
+                }
+                else
+                {
+                    Console.WriteLine(@"Error in Core Files, Please Clean the DLL Data, find it in AimtecLoader\Data\System\BelieverCore.dll");
+                }
+            }
+        }
+    }
+}
---
+++ Believer/README.md
@@ -0,0 +1,16 @@
+Support Champion: Blitzcrank, Caitlyn, Draven, Ezreal, Fiora, Kalista, KogMaw, Tristana, Twitch, Varus, Viktor <br>
+Support Utility: BaseUlt, RandomUlt <br>
+Support Event: Anti Gapcloser, Interrupt Spell <br>
+Prediction: Believer Prediction <br>
+Orbwalker: Believer Orbwalker <br>
+ <br>
+----------------------------- <br>
+Credit:  <br>
+Beaving -> for BaseUlt (i do some change for damage calculate and hp prediction)  <br>
+badao -> i learn something from him  <br>
+Brian -> his brain and his Special Evade Part <br>
+Eskor -> his Evade Part  <br>
+Sebby -> his Lag System so good!  <br>
+Soresu -> for RandomUlt  <br>
+And some people help me at LS  <br>
+of course include my user!  <br>
